13588bffa362cb8648111da0b3d37bb41d48a5ff
iJustinLA <47259287+iJustinLA@users.noreply.github.com>
2023-06-24 16:36:32 -0400

    db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://ijustinla:<Murdock09!>@cluster0.ygqz73d.mongodb.net/
+  "URI": "mongodb+srv://ijustinla:Murdock09!@cluster0.ygqz73d.mongodb.net/
 "
 };
